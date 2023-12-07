25240787c91f238cb58b5d06ff183d7e5c199add
Roger Esteve <rogeresteve30@gmail.com>
2023-12-07 03:22:21 +0100

    home button

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,6 +1,7 @@
 import { apiGET } from "@/utils/apiUtils";
 import { checkToken, deleteCookie } from "@/utils/cookieUtils";
 import { redirect } from "@/utils/webUtils";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Box, Button } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
@@ -23,7 +24,8 @@ export const Header = ({onInit} : {onInit: (logged: boolean) => void}) => {
         <Box mb={5}>
             {logged &&
                 <Box>
-                    <Button size={"sm"} colorScheme='red' onClick={logout}>Log out</Button>
+                    <Button size={"sm"} onClick={() => redirect("/")}><ArrowBackIcon></ArrowBackIcon></Button>
+                    <Button ml={3} size={"sm"} colorScheme='red' onClick={logout}>Log out</Button>
                     <Box as={"span"} ml={3}>User: { localStorage.getItem("username") }</Box>
                 </Box>
             }
